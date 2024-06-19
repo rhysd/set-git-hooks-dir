761fe7962b8ef23b36b73179e3b657253da0f751
rhysd <lin90162@yahoo.co.jp>
2024-06-20 01:45:35 +0900

    set Git hooks directory on `postinstall` hook

--- npm/index.ts
+++ npm/index.ts
@@ -1,24 +1,41 @@
-import { env, argv, exit } from 'node:process';
+import { env, cwd } from 'node:process';
 import { spawnSync as spawn } from 'node:child_process';
-import { statSync as stat } from 'node:fs';
-import { normalize } from 'node:path';
+import { statSync as stat, existsSync as exists } from 'node:fs';
+import { dirname, join } from 'node:path';
 
-export function setGitHooksDir(dirPath: string) {
-    const git = env['SET_GIT_HOOKS_DIR_GIT'] || 'git';
-    const dir = normalize(dirPath);
-    if (!stat(dir).isDirectory()) {
-        throw new Error(`The path is not a directory: ${dir}`);
-    }
-    const { status, stderr } = spawn(git, ['config', 'core.hooksPath', dir], { env: {} });
-    if (status === null || status > 0) {
-        throw new Error(`\`${git} config core.hooksPath ${dir}\` failed with status ${status}: ${stderr.toString()}`);
+function isDir(path: string): boolean {
+    try {
+        return stat(path).isDirectory();
+    } catch(err) {
+        return false;
     }
 }
 
-export function main() {
-    if (argv.length <= 2 || argv.includes('--help') || argv.includes('-h')) {
-        console.log('USAGE: npx set-git-hooks-dir path/to/hooks/dir\n\nSee https://github.com/rhysd/set-git-hooks-dir#readme');
-        exit(1);
+export function setGitHooksDir(dirName: string) {
+    const workingDir = cwd();
+    let cur = workingDir;
+    while (true) {
+        const hooksDir = join(cur, dirName);
+        if (isDir(hooksDir) && exists(join(cur, '.git'))) {
+            break; // Found
+        }
+        const parent = dirname(cur);
+        if (parent === cur) {
+            throw new Error(`Git hooks directory ${dirName} was not found in ${workingDir}`);
+        }
+        cur = parent;
     }
-    setGitHooksDir(argv[2]);
+
+    const git = env['SET_GIT_HOOKS_DIR_GIT'] || 'git';
+    const { status, stderr } = spawn(git, ['config', 'core.hooksPath', dirName], { env: {} });
+    if (status === null || status > 0) {
+        throw new Error(`\`${git} config core.hooksPath ${dirName}\` failed with status ${status}: ${stderr.toString()}`);
+    }
+}
+
+try {
+    setGitHooksDir('.git-hooks');
+    console.log('Set .git-hooks directory to core.hooksPath');
+} catch(err) {
+    console.error(err); // Do not make this script fail
 }
